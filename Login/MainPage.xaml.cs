6b498b0c1480176c856e2d9b02a1d2e9001aac95
Büşra Çakmakcı <busracakmakci95@gmail.com>
2024-06-19 18:16:25 +0300

    Add project files.

---
+++ Login/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Login
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string nickName = nickNameEntry.Text;
+
+            if (IsValidWalletInfo(nickName))
+            {
+                DisplayAlert("Başarılı", $"Giriş başarılı: {nickName}", "Tamam");
+            }
+            else
+            {
+                DisplayAlert("Hata", "Geçersiz.", "Tekrar Dene");
+            }
+        }
+
+        private bool IsValidWalletInfo(string walletInfo)
+        {
+            // Basit bir doğrulama, burada gerçek bir doğrulama uygulanmalıdır
+            return !string.IsNullOrWhiteSpace(walletInfo) && walletInfo.Length > 10;
+        }
+    }
+ }
+
+
